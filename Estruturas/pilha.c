94be0e2f8df523e9a4ff72e382989f190ed204f0
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-10-11 16:03:24 -0300

    Update pilha.c

--- Estruturas/pilha.c
+++ Estruturas/pilha.c
@@ -1,50 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "pilha.h"
 
-#define MAX 5
+#define MAX 100
+#define TAM 50
 
-int pilha[MAX];
+char pilha[MAX][TAM];
 int topo=-1;
 
-void push (int x){
-if (topo==MAX-1){
-fprintf(stderr,"Stack Overflow\n");
-exit(1);
+void push(char*x){
+    if (topo==MAX-1){
+            fprintf(stderr,"Stack Overflow\n");
+    exit(1);
+    }
+    else{
+        topo++;
+        strcpy(pilha[topo],x);
+    }
 }
-else{
-topo++;
-pilha[topo]=x;
-}
-
-int pop(void){
-
+char* pop(void){
 if(topo==-1){
-fprintf(stderr,"Stack Overflow");
-exit(1);
+    fprintf(stderr,"Stack Overflow\n");
+    exit(1);
 }
-
 else{
-return pilha[topo--];
+        printf("removido:%s\n",pilha[topo]);
+    return pilha[topo--];
 }
-
+}
 void mostrar(void){
-int i;
-printf("----------Pilha------------\n);
-for(i=0;i<=topo;i++){
-printf("[%i]",pilha[i]);
-}
-printf("---------------------------\n);
-}
+    printf("------Conteudo da pilha--------\n");
+for(int u=0;u<=topo;u++){
+        printf("[%s]\n",pilha[u]);
 
+}
+printf("-----------------------------------\n");
+}
 void destruir(void){
-int o;
-for(o=0;o<=topo;o++){
-pop();
+    for(int q=q;q<=topo;q++){
+    pop();
 }
-topo=-1;
+printf("A pilha foi esvaziada\n");
+topo==-1;
 }
-
-
-
-
-
