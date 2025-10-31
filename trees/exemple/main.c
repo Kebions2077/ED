ed3dd653ccc493f3e256a0ba1b495bea099ae6a9
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-10-31 16:22:41 -0300

    Update main.c

--- trees/exemple/main.c
+++ trees/exemple/main.c
@@ -5,9 +5,22 @@
 
 int main(void){
 node *root=NULL;
-ins(root,10);
-ins(root,9);
-ins(root,20);
-show(root);
-return 0;
+int op,int x;
+op=x=0;
+  while(op!=6){
+    printf("---------------Binary Search Tree------------------\n");
+    printf("1)Add\n3)Show\n4)Crescent\n5)Descrescent\n6)Exit");
+    printf("---------------------------------------------------\n");
+    printf("Enter An Option:\n");
+    scanf("%i",&op);
+    switch(op){
+      case 1:
+        printf("----------Add-----------\n");
+      printf("Enter a number to add");
+      scanf("%i",&x);
+      ins(root,x);
+      break;
+      case 2:
+        crescent(root)
+      
 }
